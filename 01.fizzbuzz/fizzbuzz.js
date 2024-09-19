a5ce5b6742e6467c2a99db16200b3856d01cd23a
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-09-19 20:03:56 +0900

    ./fizzbuzz.jsで実行できるようにし、3,5の公倍数でFizzBuzzが出力するようにしました

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
+
 for (let num = 1; num <= 20; num++) {
-  if (num % 15 === 0) {
+  if (num % 3 === 0 && num % 5 === 0) {
     console.log("FizzBuzz");
   } else if (num % 3 === 0) {
     console.log("Fizz");
