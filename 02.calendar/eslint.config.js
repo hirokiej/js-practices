5a4dc74b1f394b1b7f44dcabf88a71898062dcb1
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-09-21 01:49:11 +0900

    ESLintを設定しました

--- 02.calendar/eslint.config.js
+++ 02.calendar/eslint.config.js
@@ -1,9 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+
 
 export default [
-  { languageOptions: { globals: globals.nodeBuiltin } },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  eslintConfigPrettier,
 ];
