0e9e672a3ababec33fe35daf53b3770877a58ab1
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-09-21 23:29:45 +0900

    月の初日から月末までの日にちだけを出力しました

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,5 +7,11 @@ const [month, day, year] = [
   today.getFullYear(),
 ];
 
+const firstDay = new Date(year, month, 1);
+const lastDay = new Date(year, month, 0);
+
 console.log(" ".repeat(6) + `${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
+for (let f = firstDay.getDate(); f <= lastDay.getDate(); f++) {
+  console.log(f);
+}
