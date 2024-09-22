57ee25e9bea1bdda7813b8e52f8dc49fcdd26890
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-09-22 17:13:10 +0900

    日にちを土曜日で改行するようにしました

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -8,9 +8,14 @@ const [month, aday, year] = [today.month() + 1, today.date(), today.year()];
 const firstDay = dayjs().startOf("month");
 const lastDay = dayjs().endOf("month");
 
+const startOfWeek = firstDay.day();
+
 console.log(" ".repeat(6) + `${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
 for (let f = firstDay.date(); f <= lastDay.date(); f++) {
   process.stdout.write(`${f}`);
+  if ((startOfWeek + f) % 7 === 0) {
+    console.log();
+  }
 }
