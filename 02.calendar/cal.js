6690a6d902eb98915adc8569a1b1b8a4055d89f8
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-09-22 17:46:15 +0900

    カレンダー通りの見た目になるように調整しました

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -13,8 +13,14 @@ const startOfWeek = firstDay.day();
 console.log(" ".repeat(6) + `${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
+process.stdout.write(" ".repeat(startOfWeek * 3)); //全角 + 半角で3スペース必要
+
 for (let f = firstDay.date(); f <= lastDay.date(); f++) {
-  process.stdout.write(`${f}`);
+  if (f < 10) {
+    process.stdout.write(` ${f} `);
+  } else {
+    process.stdout.write(`${f} `);
+  }
   if ((startOfWeek + f) % 7 === 0) {
     console.log();
   }
