d39f2059a1832fda5ad002086895bd2e8d21f9ce
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-06 12:56:59 +0900

    エラーなしのcallbackプログラムを作成しました

---
+++ 03.asynchronous/callback.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:");
+
+db.run(
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL)",
+  () => {
+    console.log("Booksテーブルを作成しました");
+    db.run("INSERT INTO books(title) VALUES(?)", "javascriptの本", () => {
+      console.log("本を追加しました。");
+      db.each("SELECT * FROM books", (_, row) => {
+        console.log(`id:${row.id}は${row.title}`);
+
+        db.run("DROP TABLE books", () => {
+          console.log("Booksテーブルを削除しました");
+          db.close();
+        });
+      });
+    });
+  },
+);
