c69101bb7374ed47699aef14f326dca3a3bb1dba
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-07 17:29:25 +0900

    promise.jsにBooksテーブルを追加した旨を示す出力を加えました

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -7,6 +7,7 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL)",
   () => {
+    console.log("Booksテーブルを作成しました");
     new Promise((resolve) => {
       db.run("INSERT INTO books(title) VALUES(?)", "javascriptの本", () => {
         console.log("本を追加しました。");
