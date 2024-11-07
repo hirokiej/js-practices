7c501c8b1d6ad7fe498652555061ac75d1b14b89
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-07 16:55:41 +0900

    エラーなしのpromiseのプログラムを作成しました

---
+++ 03.asynchronous/promise.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:");
+
+db.run(
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL)",
+  () => {
+    new Promise((resolve) => {
+      db.run("INSERT INTO books(title) VALUES(?)", "javascriptの本", () => {
+        console.log("本を追加しました。");
+        resolve();
+      });
+    })
+      .then(() => {
+        new Promise((resolve) => {
+          db.each("SELECT * FROM books", (_, row) => {
+            console.log(`id:${row.id}は${row.title}`);
+            resolve();
+          });
+        });
+      })
+      .then(() => {
+        new Promise((resolve) => {
+          db.run("DROP TABLE books", () => {
+            console.log("Booksテーブルを削除しました");
+            resolve();
+          });
+        });
+      })
+      .then(() => {
+        new Promise((resolve) => {
+          db.close();
+          resolve();
+        });
+      });
+  },
+);
