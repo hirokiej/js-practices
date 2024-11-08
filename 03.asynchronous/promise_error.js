0bbf127658ece2d1baf6f92a247e33bc5a7fa467
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-08 12:48:33 +0900

    promiseを利用したエラーありのプログラムを作成しました

---
+++ 03.asynchronous/promise_error.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:");
+
+db.run(
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL)",
+  () => {
+    console.log("Booksテーブルを作成しました");
+    new Promise((resolve, reject) => {
+      db.run("INSERT INTO books(title) VALUES(?)", null, (err) => {
+        if (err) {
+          console.log("データ追加エラー");
+          reject(err);
+        } else {
+          console.log("本を追加しました。");
+          resolve();
+        }
+      });
+    })
+      .catch((err) => {
+        return new Promise((resolve, reject) => {
+          db.each("SELECT * FROM members", (err, row) => {
+            if (err) {
+              console.log("データ取得エラー");
+              resolve();
+            } else {
+              console.log(`id:${row.id}は${row.title}`);
+              resolve();
+            }
+          });
+        });
+      })
+      .then(() => {
+        return new Promise((resolve) => {
+          db.run("DROP TABLE books", () => {
+            console.log("Booksテーブルを削除しました");
+            resolve();
+          });
+        });
+      })
+      .then(() => {
+        return new Promise((resolve) => {
+          db.close();
+          resolve();
+        });
+      });
+  },
+);
