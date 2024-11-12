927b91432a532b9678d4daea150c5f05cbb123b0
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-13 00:30:27 +0900

    .runに対応する関数を作成しました

--- 03.asynchronous/promise/promise_function.js
+++ 03.asynchronous/promise/promise_function.js
@@ -2,6 +2,18 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
+export const dbRun = () => {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
 export const dbClose = () => {
   new Promise((resolve) => {
     db.close();
