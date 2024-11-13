a60d4182b65d6fdd3f18257bb0ca1690a5b53e66
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-13 11:07:07 +0900

    関数dbRunに引数を追加しました

--- 03.asynchronous/promise/promise_function.js
+++ 03.asynchronous/promise/promise_function.js
@@ -2,7 +2,7 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-export const dbRun = () => {
+export const dbRun = (query, params) => {
   return new Promise((resolve, reject) => {
     db.run(query, params, (err) => {
       if (err) {
