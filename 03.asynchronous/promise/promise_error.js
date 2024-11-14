73bffcfe52555bc9325d32d7fea0223f49dd756c
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-14 20:41:58 +0900

    function.jsのファイル名と場所が変わったのでインポート先を変更しました

--- 03.asynchronous/promise/promise_error.js
+++ 03.asynchronous/promise/promise_error.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
-import { dbRun, dbEach, dbClose } from "./promise_function.js";
+import { dbRun, dbEach, dbClose } from "../function.js";
 
 const db = new sqlite3.Database(":memory:");
 
