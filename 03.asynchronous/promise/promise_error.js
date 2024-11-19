bfcd5d1c876afe8594db503c82ba34b8662a5391
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-20 00:01:25 +0900

    promise_error.jsでdbをimportしました

--- 03.asynchronous/promise/promise_error.js
+++ 03.asynchronous/promise/promise_error.js
@@ -1,31 +1,32 @@
 #!/usr/bin/env node
 
-import { dbRun, dbEach, dbClose } from "../function.js";
+import { db, dbRun, dbEach, dbClose } from "../function.js";
 
 dbRun(
+  db,
   "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
 )
   .then(() => {
     console.log("Booksテーブルを作成しました");
-    return dbRun("INSERT INTO books(title) VALUES(?)", null);
+    return dbRun(db, "INSERT INTO books(title) VALUES(?)", null);
   })
   .then(() => {
     console.log("本を追加しました。");
-    return dbEach("SELECT * FROM books");
+    return dbEach(db, "SELECT * FROM books");
   })
   .catch(() => {
     console.error("データ追加エラー");
-    return dbEach("SELECT * FROM members");
+    return dbEach(db, "SELECT * FROM members");
   })
   .then((row) => {
     console.log(`id:${row.id}は${row.title}`);
-    return dbRun("DROP TABLE books");
+    return dbRun(db, "DROP TABLE books");
   })
   .catch(() => {
     console.error("データ取得エラー");
-    return dbRun("DROP TABLE books");
+    return dbRun(db, "DROP TABLE books");
   })
   .then(() => {
     console.log("テーブルを削除しました");
-    return dbClose();
+    return dbClose(db);
   });
