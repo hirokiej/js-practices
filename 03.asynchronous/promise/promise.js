faf2af40546fc51780a548bd59aacf62edbcf0fb
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-20 00:00:02 +0900

    promise.jsでdbをimportしました

--- 03.asynchronous/promise/promise.js
+++ 03.asynchronous/promise/promise.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 
-import { dbRun, dbEach, dbClose } from "../function.js";
+import { db, dbRun, dbEach, dbClose } from "../function.js";
 
 dbRun(
+  db,
   "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
 )
   .then(() => {
     console.log("Booksテーブルを作成しました");
-    dbRun("INSERT INTO books(title) VALUES(?)", "JavaScriptの本");
+    dbRun(db, "INSERT INTO books(title) VALUES(?)", "JavaScriptの本");
   })
   .then(() => {
     console.log("本を追加しました。");
-    return dbEach("SELECT * FROM books");
+    return dbEach(db, "SELECT * FROM books");
   })
   .then((row) => {
     console.log(`id:${row.id}は${row.title}`);
-    return dbRun("DROP TABLE books");
+    return dbRun(db, "DROP TABLE books");
   })
   .then(() => {
     console.log("テーブルを削除しました");
-    return dbClose();
+    return dbClose(db);
   });
