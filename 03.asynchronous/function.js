3248a986fc70e5420687166aac385080cd0e2960
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-11-19 23:58:47 +0900

    定数dbにexportをつけ、他ファイルでimportした時に利用できるようにしました

--- 03.asynchronous/function.js
+++ 03.asynchronous/function.js
@@ -1,6 +1,6 @@
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(":memory:");
+export const db = new sqlite3.Database(":memory:");
 
 export const dbRun = (db, query, params) => {
   return new Promise((resolve, reject) => {
