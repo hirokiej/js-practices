8c169c81440c4e93255f4925e0217e81ece8e260
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-12-02 00:18:27 +0900

    独自の関数を削除しasync関数内に処理をまとめました

--- 03.asynchronous/async_await/async_await_error.js
+++ 03.asynchronous/async_await/async_await_error.js
@@ -2,51 +2,25 @@
 
 import { db, dbRun, dbAll, dbClose } from "../function.js";
 
-const createTable = () => {
-  return dbRun(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
-  ).then(() => {
-    console.log("Booksテーブルを作成しました");
-  });
-};
-
-const insertBook = (title) => {
-  return dbRun(db, "INSERT INTO books(title) VALUES(?)", title).then(() => {
-    console.log("本を追加しました。");
-  });
-};
-
-const outputBook = () => {
-  return dbAll(db, "SELECT * FROM members").then((row) => {
-    console.log(`id:${row.id}は${row.title}`);
-  });
-};
-
-const dropTable = () => {
-  return dbRun(db, "DROP TABLE books").then(() => {
-    console.log("Booksテーブルを削除しました");
-  });
-};
-
-const closeDatabase = () => {
-  dbClose(db);
-};
-
 const main = async () => {
   try {
-    await createTable();
-    await insertBook(null);
+    await dbRun(
+      db,
+      "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
+    );
+    console.log("テーブルを作成しました");
+    await dbRun(db, "INSERT INTO books(title) VALUES(?)' null");
   } catch {
     console.error("データ追加エラー");
   }
   try {
-    await outputBook();
+    await dbAll(db, "SELECT * FROM members");
   } catch {
     console.error("データ取得エラー");
   }
-  await dropTable();
-  await closeDatabase();
+  await dbRun(db, "DROP TABLE books");
+  await dbClose(db);
+  console.log("テーブルを削除しました");
 };
 
 main();
