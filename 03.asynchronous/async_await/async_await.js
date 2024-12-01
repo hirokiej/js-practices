48ea6bf80540eb26f49bcec32a8abeba5f4d9013
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-12-01 23:55:38 +0900

    Top-level awaitを使用し、async関数を削除しました

--- 03.asynchronous/async_await/async_await.js
+++ 03.asynchronous/async_await/async_await.js
@@ -2,25 +2,21 @@
 
 import { db, dbRun, dbAll, dbClose } from "../function.js";
 
-const main = async () => {
-  await dbRun(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
-  );
-  console.log("テーブルを作成しました");
-  const result = await dbRun(
-    db,
-    "INSERT INTO books(title) VALUES(?)",
-    "Javascriptの本",
-  );
-  console.log("本を追加しました");
-  console.log(`idは${result.lastID}です`);
-  const rows = await dbAll(db, "SELECT * FROM books");
-  rows.forEach((row) => {
-    console.log(`${row.id}は${row.title}です`);
-  });
-  await dbRun(db, "DROP TABLE books");
-  await dbClose(db);
-};
-
-main();
+await dbRun(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
+);
+console.log("テーブルを作成しました");
+const result = await dbRun(
+  db,
+  "INSERT INTO books(title) VALUES(?)",
+  "Javascriptの本",
+);
+console.log("本を追加しました");
+console.log(`idは${result.lastID}です`);
+const rows = await dbAll(db, "SELECT * FROM books");
+rows.forEach((row) => {
+  console.log(`${row.id}は${row.title}です`);
+});
+await dbRun(db, "DROP TABLE books");
+await dbClose(db);
