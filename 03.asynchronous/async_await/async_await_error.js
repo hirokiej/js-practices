fc772f74d40ad1afb27b39327fe1b3fd6700b0ae
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-12-05 18:00:46 +0900

    Top-level awaitを使用しasync関数を削除しました

--- 03.asynchronous/async_await/async_await_error.js
+++ 03.asynchronous/async_await/async_await_error.js
@@ -2,33 +2,29 @@
 
 import { db, dbRun, dbAll, dbClose } from "../function.js";
 
-const main = async () => {
-  try {
-    await dbRun(
-      db,
-      "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
-    );
-    console.log("テーブルを作成しました");
-    await dbRun(db, "INSERT INTO books(title) VALUES(?)", null);
-  } catch (err) {
-    if (err instanceof Error && err.code === "SQLITE_CONSTRAINT") {
-      console.error("データ追加エラー", err.message);
-    } else {
-      throw err;
-    }
+try {
+  await dbRun(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
+  );
+  console.log("テーブルを作成しました");
+  await dbRun(db, "INSERT INTO books(title) VALUES(?)", null);
+} catch (err) {
+  if (err instanceof Error && err.code === "SQLITE_CONSTRAINT") {
+    console.error("データ追加エラー", err.message);
+  } else {
+    throw err;
   }
-  try {
-    await dbAll(db, "SELECT * FROM members");
-  } catch (err) {
-    if (err instanceof Error && err.code == "SQLITE_ERROR") {
-      console.error("データ取得エラー", err.message);
-    } else {
-      throw err;
-    }
+}
+try {
+  await dbAll(db, "SELECT * FROM members");
+} catch (err) {
+  if (err instanceof Error && err.code == "SQLITE_ERROR") {
+    console.error("データ取得エラー", err.message);
+  } else {
+    throw err;
   }
-  await dbRun(db, "DROP TABLE books");
-  await dbClose(db);
-  console.log("テーブルを削除しました");
-};
-
-main();
+}
+await dbRun(db, "DROP TABLE books");
+await dbClose(db);
+console.log("テーブルを削除しました");
