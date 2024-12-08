4438217db465b48f18e77a38c6943a0ef44202c9
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-12-08 15:56:18 +0900

    例外が送出される想定の箇所にtryを囲みました

--- 03.asynchronous/async_await/async_await_error.js
+++ 03.asynchronous/async_await/async_await_error.js
@@ -2,12 +2,12 @@
 
 import { db, dbRun, dbAll, dbClose } from "../db_functions.js";
 
+await dbRun(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
+);
+console.log("Booksテーブルを作成しました");
 try {
-  await dbRun(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
-  );
-  console.log("Booksテーブルを作成しました");
   await dbRun(db, "INSERT INTO books(title) VALUES(?)", null);
 } catch (err) {
   if (err instanceof Error && err.code === "SQLITE_CONSTRAINT") {
