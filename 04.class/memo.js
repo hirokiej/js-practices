270b1f5cc70f64613fbd46ea9d7013d9357fdb6c
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-12-26 16:45:15 +0900

    関数listMemos作成し、OperateInterfaceクラス内で処理を完結するようにしました

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,16 +2,12 @@ import MemoOperation, { db } from "./memoOperation.js";
 import OperateInterface from "./operateInterface.js";
 
 const memoOperation = new MemoOperation();
-const operateInterface = new OperateInterface();
+const operateInterface = new OperateInterface(memoOperation);
 
 const firstArg = process.argv[2];
 
 if (firstArg === "-l") {
-  memoOperation.fetchMemos().then((rows) => {
-    rows.forEach((row) => {
-      console.log(`${row.content.split("\n")[0]}`);
-    });
-  });
+  operateInterface.listMemos();
 } else if (firstArg === "-r") {
   operateInterface.selectMemoFromList((result) => {
     console.log(result);
