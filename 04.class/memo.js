772dd03fa25e0e4d05081fd12c437ddd5bcb7f51
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-12-26 17:25:20 +0900

    関数readMemosを作成し、listMemosと対称性を持たせました

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -9,9 +9,7 @@ const firstArg = process.argv[2];
 if (firstArg === "-l") {
   operateInterface.listMemos();
 } else if (firstArg === "-r") {
-  operateInterface.selectMemoFromList((result) => {
-    console.log(result);
-  });
+  operateInterface.readMemos();
 } else if (firstArg === "-d") {
   operateInterface.selectMemoFromList((result) => {
     memoOperation.deleteMemo(result);
