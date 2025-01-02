442fb9c8ffb64c7c01bce1525886aa98923b606c
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-01-03 01:58:53 +0900

    SQL文をバッククォートで囲み、改行を追加して可読性を向上させました

--- 04.class/memoOperation.js
+++ 04.class/memoOperation.js
@@ -5,7 +5,9 @@ export const db = new sqlite3.Database("memo.db");
 export default class MemoOperation {
   constructor() {
     db.run(
-      "CREATE TABLE IF NOT EXISTS memo(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)",
+      `CREATE TABLE IF NOT EXISTS memo(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT)`,
     );
   }
 
