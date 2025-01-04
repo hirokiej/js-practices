81b5d81f4064f5f14492fd62fe8b72cfebaa55d9
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-01-04 16:47:29 +0900

    無効な引数を持った場合にエラーを出力するよう修正しました

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -6,18 +6,23 @@ import OperateInterface from "./operateInterface.js";
 const memoOperation = new MemoOperation();
 const operateInterface = new OperateInterface(memoOperation);
 
+const validOptions = ["-l", "-r", "-d"];
 const firstArg = process.argv[2];
 
 async function main() {
   try {
-    if (firstArg === "-l") {
+    if (!firstArg) {
+      await operateInterface.writeMemo();
+    } else if (!validOptions.includes(firstArg)) {
+      console.error(
+        "Invalid option: Please use -l(list), -r(read) or -d(delete)",
+      );
+    } else if (firstArg === "-l") {
       await operateInterface.listMemos();
     } else if (firstArg === "-r") {
       await operateInterface.readMemos();
     } else if (firstArg === "-d") {
       await operateInterface.deleteMemo();
-    } else {
-      await operateInterface.writeMemo();
     }
   } finally {
     memoOperation.dbClose();
